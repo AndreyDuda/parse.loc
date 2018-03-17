ccbd9e00d1b24d1c80b633d234bd3354e7de65f4
Duda <andreyduda88@gmail.com>
2018-03-17 14:08:56 +0200

    layouts

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,7 +4,89 @@ namespace Kino\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class IndexController extends Controller
+class IndexController extends SiteController
 {
-    //
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->template = env('THEME') . '.index';
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Список
+        return $this->renderOutput();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //Добовление форма
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //ПОСТ сограние записей
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // отбражение некоторого данного
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //Форма редактирование
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //Обновление информации
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //Удаление элеиента
+    }
 }
--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -7,4 +7,17 @@ use Illuminate\Http\Request;
 class SiteController extends Controller
 {
     //
+    protected $site_rep;
+
+    protected $template;
+    protected $vars = array();
+
+    public function __construct()
+    {
+    }
+
+    protected function renderOutput()
+    {
+        return view($this->template)->with($this->vars);
+    }
 }
